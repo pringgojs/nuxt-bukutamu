3801477cd39f6d7135c807000ace49867ba5a382
Pringgo Juni S <odyinggo@gmail.com>
2022-02-05 11:54:54 +0700

    wip: setup layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-app-bar color="#6A76AB" height="300"> </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
---
+++ pages/form.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-row justify="center" style="margin-top: -250px">
+      <v-card width="400">
+        <v-responsive>
+          <v-card-title> {{ title }} </v-card-title>
+          <v-card-text>Mohon lengkapi form dibawah ini</v-card-text>
+          <v-col>
+            <v-text-field label="Nama Anda" outlined></v-text-field>
+            <v-text-field label="Instansi/SKPD" outlined></v-text-field>
+            <v-text-field
+              type="number"
+              label="No. Telepon"
+              outlined
+            ></v-text-field>
+            <v-col>
+              <v-btn
+                v-on:click="openCam"
+                class="white--text"
+                fab
+                icon
+                small
+                color="orange"
+              >
+                <v-icon dark> mdi-camera </v-icon>
+              </v-btn>
+            </v-col>
+            <v-btn color="primary" elevation="2">Simpan</v-btn>
+          </v-col>
+        </v-responsive>
+      </v-card>
+      <v-card class="ml-4" height="100" flat>
+        <v-responsive class="justify-center">
+          <v-img
+            src=""
+            height="125"
+            width="125"
+            style="border: 2px solid #fff; background: #6a76ab"
+          ></v-img>
+        </v-responsive>
+      </v-card>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Buku Tamu Kominfo',
+    }
+  },
+  methods: {
+    openCam() {
+      alert('haloo')
+    },
+  },
+}
+</script>
