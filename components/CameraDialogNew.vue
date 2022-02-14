fa66541fbcccb4a9306dfca18af67bbcdd860226
pringgojs <odyinggo@gmail.com>
2022-02-14 12:30:24 +0700

    update UI camera

---
+++ components/CameraDialogNew.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-row justify="space-around">
+    <v-col cols="auto">
+      <v-dialog v-model="dialog" transition="dialog-top-transition" max-width="600">
+        <template>
+          <v-card>
+            <v-toolbar color="primary" dark>Ambil Foto</v-toolbar>
+            <!-- <v-card-text>
+              <div class="text-h2 pa-12">Hello world!</div>
+            </v-card-text>-->
+            <v-row justify="center" class="mt-10 pb-10">
+              <v-card max-width="444" height="auto">
+                <video ref="video" id="video" width="100%" autoplay></video>
+
+                <v-card-actions>
+                  <v-btn
+                    style="margin-left:0px"
+                    id="snap"
+                    v-on:click="capture()"
+                    elevation="2"
+                    color="primary "
+                  >Capture</v-btn>
+                </v-card-actions>
+              </v-card>
+              <v-card max-width="344" class="ml-5" elevation="0">
+                <canvas ref="canvas" id="canvas" width="640" height="380" style="display:none"></canvas>
+              </v-card>
+              <!-- <v-col cols="4" id="my_camera" style="padding-left:5px; border:1px solid #eee">hasd</v-col>
+              <v-col cols="4" id="results" style="border:1px solid #eee">asd</v-col>-->
+            </v-row>
+            <v-card-actions class="justify-end"></v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+    </v-col>
+  </v-row>
+</template>
+
+
+<script>
+export default {
+    data: () => ({
+        dialog: false,
+        resolve : null,
+        reject : null,
+        video: {},
+        canvas: {},
+        captures: []
+    }),
+    methods: {
+        open(){
+            return new Promise((resolve, reject)=>{
+                this.resolve = resolve
+                this.reject = reject
+                this.dialog = true;
+                this.canvas = {}
+            })
+        },
+        accept(str) {
+            this.dialog = false;
+            this.resolve(str);
+        },
+        cancel(str) {
+            this.dialog = false;
+            this.reject(str);
+        },
+
+        capture() {
+            this.canvas = this.$refs.canvas;
+            var context = this.canvas
+                .getContext("2d")
+                .drawImage(this.video, 0, 0, 640, 380);
+            // this.captures.push(canvas.toDataURL("image/png"));
+            console.log(canvas.toDataURL("image/png"))
+
+            this.$store.commit('setImagePath', canvas.toDataURL("image/png"))
+            this.dialog = false;
+        },
+    },
+    updated: function () {
+        this.$nextTick(function () {
+            let app = this;
+            console.log(this.$refs.video)
+            app.video = this.$refs.video;
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({video: true,audio: false}).then(stream => {
+                    app.video.srcObject = stream;
+                    app.video.play();
+                });
+            }
+        })
+    },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,88 +1,179 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
+  <div>
+    <v-row justify="center" style="margin-top: -250px">
+      <v-card width="1000">
+        <v-responsive>
+          <v-row>
+            <v-col cols="6" class="pa-10" style>
+              <v-card-title style="margin-left: -10px">{{ title }}</v-card-title>
+              <v-card-text style="margin-left: -10px">Mohon lengkapi form dibawah ini</v-card-text>
+              <!-- <vue-web-camp ref="webcam" /> -->
+              <v-text-field :label="labels.name" v-model="form.name" outlined></v-text-field>
+              <v-text-field :label="labels.institution" v-model="form.institution" outlined></v-text-field>
+              <v-text-field type="number" :label="labels.phone" v-model="form.phone" outlined></v-text-field>
+              <v-row>
+                <v-col>
+                  <v-btn v-on:click="openCam" class="ma-2 white--text" color="orange">
+                    <v-icon dark left>mdi-camera</v-icon>Ambil Foto
+                  </v-btn>
+                </v-col>
+                <v-col>
+                  <v-img
+                    height="150"
+                    width="150"
+                    :src="
+                      this.$store.state.captureImgPath
+                        ? this.$store.state.captureImgPath
+                        : '/no-img.png'
+                    "
+                  ></v-img>
+                </v-col>
+              </v-row>
+              <v-btn color="primary" v-on:click="save()" elevation="2">Simpan</v-btn>
+            </v-col>
+
+            <v-col cols="6" class="pa-10">
+              <Gallery />
+            </v-col>
+          </v-row>
+        </v-responsive>
       </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            pringgo
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+    </v-row>
+
+    <!-- dialog -->
+    <CameraDialogNew ref="dlg" />
+
+    <!-- snackbar -->
+    <v-snackbar v-model="snackbar" :timeout="snackbarTimeout" color="red accent-2">
+      {{ snackbarText }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="light accent-2" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import CameraDialogNew from '../components/CameraDialogNew.vue'
+import Gallery from '../components/Gallery.vue'
 export default {
-  name: 'IndexPage',
+  components: {
+    CameraDialogNew,
+    Gallery,
+  },
+  data() {
+    return {
+      hasil: '',
+      imgPath: null,
+      title: 'Daftar Hadir Hari Pers Nasional (HPN) 2022',
+      form: {
+        name: null,
+        institution: null,
+        phone: null,
+      },
+      labels: {
+        name: 'Nama Anda',
+        institution: 'Instansi / SKPD',
+        phone: 'No. Telepon',
+      },
+      // guests: [
+      //   { header: 'Real Time User' },
+      //   {
+      //     avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+      //     name: 'Pringgo Juni Saputro',
+      //     institution: 'Dinas Kominfo',
+      //     phone: '085736676648',
+      //   },
+      //   {
+      //     avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      //     name: 'Handi Setyawan',
+      //     institution: 'Dinas Perhubungan',
+      //     phone: '085736676648',
+      //   },
+      //   {
+      //     avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+      //     name: 'Jhon Doe',
+      //     institution: 'Dinas Pekerjaan Umum',
+      //     phone: '085736676648',
+      //   },
+      // ],
+      snackbar: false,
+      snackbarText: 'My timeout is set to 2000.',
+      snackbarTimeout: 2000,
+    }
+  },
+  methods: {
+    openCam() {
+      this.$refs.dlg
+        .open()
+        .then((res) => {
+          this.hasil = res
+        })
+        .catch((res) => {
+          this.hasil = res
+        })
+    },
+    save() {
+      console.log(this.$config.apiUrl)
+      if (this.form.name == null) {
+        this.snackbar = true
+        this.snackbarText = 'Nama belum dilengkapi'
+        return
+      }
+
+      if (this.form.institution == null) {
+        this.snackbar = true
+        this.snackbarText = 'Instansi/SKPD belum dilengkapi'
+        return
+      }
+
+      if (this.form.phone == null) {
+        this.snackbar = true
+        this.snackbarText = 'No. Telepon belum dilengkapi'
+        return
+      }
+
+      if (this.$store.state.captureImgPath == null) {
+        this.snackbar = true
+        this.snackbarText = 'Foto wajah belum dilengkapi'
+        return
+      }
+      let addGuest = {
+        avatar: this.$store.state.captureImgPath,
+        name: this.form.name,
+        institution: this.form.institution,
+        phone: this.form.phone,
+      }
+
+      let app = this
+      axios
+        .post(app.$config.apiUrl + '/guest/store', addGuest, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+          },
+        })
+        .then(function (response) {
+          app.$store.commit('addGuestInit', response.data)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+
+      this.resetForm()
+      this.snackbar = true
+      this.snackbarText = 'Data berhasil disimpan'
+
+      // this.guests.push({ ...addGuest })
+    },
+
+    resetForm() {
+      this.form.name = ''
+      this.form.institution = ''
+      this.form.phone = ''
+      this.$store.commit('setImagePath', '')
+    },
+  },
 }
 </script>
